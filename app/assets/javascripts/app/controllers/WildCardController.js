95d694378f2acfe9eb1377610582d1fd25481897
bacitracin <tracyctran@gmail.com>
2016-06-25 21:49:18 -0700

    querying unofficial opentable API is successful

--- app/assets/javascripts/app/controllers/WildCardController.js
+++ app/assets/javascripts/app/controllers/WildCardController.js
@@ -4,8 +4,21 @@ openTableApp.controller('WildCardController', ['$scope', '$resource', 'CityServi
         $scope.city = CityService.city;
 
         $scope.openTableAPI = $resource
-        ('http://opentable.herokuapp.com/api/restaurants?per_page=100' + $scope.city, {
-          callback: "JSON_CALLBACK" }, {get: {method: "JSONP"}});
+        ('http://opentable.herokuapp.com/api/restaurants', {
+          callback: "JSON_CALLBACK" }, {get: {method: "JSONP" }});
 
-        $scope.searchResult = $scope.openTableAPI.get({city: $scope.city});
+        $scope.openTableSearchResult = $scope.openTableAPI.get({city: $scope.city, per_page: 100});
+
+        //console.log($scope.openTableSearchResult);
+
+        // Random number for picking the restaurant
+       // var randomNumber = Math.random() * (99 - 0) + 0;
+
+       // var randomRestaurant = $scope.openTableSearchResult['restaurants'][randomNumber];
+       // randomRestaurant.name = randomRestaurant.name;
+       // randomRestaurant.address = randomRestaurant.address;
+       // randomRestaurant.city = randomRestaurant.city;
+       // randomRestaurant.image_url = randomRestaurant.image_url;
+       // randomRestaurant.reserve_url = randomRestaurant.reserve_url;
+
   }]);
