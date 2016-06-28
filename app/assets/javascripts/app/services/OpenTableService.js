19d9d47b6dfe16ae6909e09f63fb390b7e249214
bacitracin <tracyctran@gmail.com>
2016-06-28 15:12:02 -0700

    remove dupe bootstrap files from bower_components folder, add OpenTableService file to Services, but not functional yet

---
+++ app/assets/javascripts/app/services/OpenTableService.js
@@ -0,0 +1,13 @@
+angular
+  .module('openTableApp')
+  .service('OpenTableService', OpenTableService);
+
+function OpenTableService($resource){
+
+  this.getResults = function(city, numResults){
+    var openTableAPI = $resource('http://opentable.herokuapp.com/api/restaurants', {
+    callback: "JSON_CALLBACK" }, {get: {method: "JSONP" }});
+    return openTableAPI.get({city: city, per_page: numResults});
+
+  }
+}
