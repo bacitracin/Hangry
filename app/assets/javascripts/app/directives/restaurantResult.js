5e022b43f4850ec2d0c31dca315f1fff0845c178
bacitracin <tracyctran@gmail.com>
2016-07-02 16:14:07 -0700

    <restaurant-result> custom directive working

--- app/assets/javascripts/app/directives/restaurantResult.js
+++ app/assets/javascripts/app/directives/restaurantResult.js
@@ -6,6 +6,7 @@ angular
 function restaurantResult() {
   return {
     restrict: 'E', //will look like <restaurant-result></restaurant-result>
-    template: '<div class="panel-body"><a href="/#/restaurants/{{restaurant.id}}">{{restaurant.name}}</a> - {{restaurant.city}}</div>'
+    template: '<div class="panel-body"><a href="#" ui-sref="restaurant({id: restaurant.id})">{{ restaurant.name}} </a> - {{ restaurant.city }} - <a href="#" ui-sref="edit({id: restaurant.id})">Edit Details</a></div>',
+    replace: true
   };
 }
