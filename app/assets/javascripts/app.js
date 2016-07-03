492af5e199e71211e7b90d3525ad32fb158a4da0
bacitracin <tracyctran@gmail.com>
2016-07-03 15:28:36 -0700

    change folder structure to feature based

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,37 +5,37 @@ var openTableApp = angular
                     // Landing page that asks for your city
                     .state('home', {
                       url: '/',
-                      templateUrl: 'app/views/home.html',
+                      templateUrl: 'app/home/home.html',
                       controller: 'HomeController as home'
                     })
                     // Hits Unofficial OpenTable API and pulls restaurant results
                     .state('home.wildcard', {
                       url: 'wildcard',
-                      templateUrl: 'app/views/wildcard.html',
+                      templateUrl: 'app/wildcard/wildcard.html',
                       controller: 'WildCardController as wildcard'
                     })
                     // This is all saved restaurants 
                     .state('restaurants', {
                       url: '/restaurants',
-                      templateUrl: 'app/views/restaurants.html',
+                      templateUrl: 'app/restaurants/views/restaurants.html',
                       controller: 'RestaurantsController as restaurants'
                     })
                     // Add a new restaurant 
                     .state('new', {  
                       url: '/new',
-                      templateUrl: 'app/views/new.html',
+                      templateUrl: 'app/restaurants/views/new.html',
                       controller: 'NewRestaurantController as newRestaurant'
                     })
                     // Show page 
                     .state('restaurant', { 
                       url: '/restaurant/:id',
-                      templateUrl: 'app/views/show.html',
+                      templateUrl: 'app/restaurants/views/show.html',
                       controller: 'ShowRestaurantController as showRestaurant'
                     })
                     // Edit restaurant details 
                     .state('edit', {
                       url:'/edit/:id',
-                      templateUrl: 'app/views/edit.html',
+                      templateUrl: 'app/restaurants/views/edit.html',
                       controller: 'EditRestaurantController as editRestaurant'
                     })
 
