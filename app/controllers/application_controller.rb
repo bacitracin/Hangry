715eb34d88eea841009e1b89ceecc35fef96c206
bacitracin <tracyctran@gmail.com>
2016-07-04 14:03:23 -0700

    trying out solutions for devise in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,28 +3,44 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  # trying this http://stackoverflow.com/questions/14734243/rails-csrf-protection-angular-js-protect-from-forgery-makes-me-to-log-out-on
+  # trying this 
+
   # null session suggested here https://github.com/lynndylanhurley/devise_token_auth/issues/398
-  protect_from_forgery with: :null_session 
+  protect_from_forgery #with: :null_session 
   after_filter :set_csrf_cookie_for_ng
 
   respond_to :html, :json
   
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_filter :set_csrf_cookie_for_ng
 
   def angular
     render 'layouts/application'
   end
 
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  def after_sign_in_path_for(resource)
+  root_path
+  end
+   
+  def after_sign_out_path_for(resource)
+  root_path
   end
 
   private
 
+  # tell devise about username 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
+
+  def set_user
+  @user||= current_user
+  end
+   
+  def set_csrf_cookie_for_ng
+  cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+   
 
  protected
 
