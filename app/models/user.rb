dcb89c8d831da207badcd79d944f303e6f74681f
bacitracin <tracyctran@gmail.com>
2016-07-07 21:36:29 -0700

    update name of scopes, fix syntax, was missing {}

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :restaurants, through: :reviews
 
+  scope :not_visited_restaurants, -> {Restaurant.all.select{|restaurant| !restaurant.users.include?(self)}}
+  scope :visited_restaurants, -> { self.restaurants }
 
 end
+
