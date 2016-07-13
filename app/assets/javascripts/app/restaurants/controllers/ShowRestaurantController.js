fc89db86d05685390619880a09b67f474935860e
bacitracin <tracyctran@gmail.com>
2016-07-12 21:44:23 -0700

    google maps is showing the correct address based on restaurant input. the issue before was i was trying to get data out of a $resource object and needed to deal with the $promise

--- app/assets/javascripts/app/restaurants/controllers/ShowRestaurantController.js
+++ app/assets/javascripts/app/restaurants/controllers/ShowRestaurantController.js
@@ -2,7 +2,7 @@ angular
   .module('openTableApp')
   .controller('ShowRestaurantController', ShowRestaurantController);
 
-function ShowRestaurantController(Restaurant, $stateParams, $scope, RestaurantService){
+function ShowRestaurantController(Restaurant, $stateParams, $scope, RestaurantService, uiGmapGoogleMapApi){
 
   var showRestaurant = this;
 
@@ -11,27 +11,23 @@ function ShowRestaurantController(Restaurant, $stateParams, $scope, RestaurantSe
   $scope.selectedRestaurant = showRestaurant.restaurant;
   
   $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-  
+  $scope.markers = [];
+
   showRestaurant.updateScope = function(){
     RestaurantService.selectedRestaurant = $scope.selectedRestaurant;
   }
-
-  var address='479 14th St San Francisco CA';
-  //var address = showRestaurant.restaurant.address + " " + showRestaurant.restaurant.city + " " + showRestaurant.restaurant.state + " " + showRestaurant.restaurant.postal_code;
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({'address':address},function(results, status){
-    if (status == google.maps.GeocoderStatus.OK){
-      restaurantLat = (results[0].geometry.location.lat());
-      restaurantLng = (results[0].geometry.location.lng());
-      $scope.map = { center: { latitude: restaurantLat, longitude: restaurantLng}, zoom: 15 };
-
-      var marker=new google.maps.Marker({
-      map: $scope.map,
-      position: results[0].geometry.location
-      });
-    }else{
-      alert(status);
-      }   
+  $scope.selectedRestaurant.$promise.then(function(data){
+    var address = (data.address + " " + data.city + " " + data.state + " " + data.postal_code);
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address},function(results, status){
+      if (status == google.maps.GeocoderStatus.OK){
+        restaurantLat = (results[0].geometry.location.lat());
+        restaurantLng = (results[0].geometry.location.lng());
+        $scope.map = { center: { latitude: restaurantLat, longitude: restaurantLng}, zoom: 15};
+      }else{
+        alert(status);
+        }   
+    });
   });
   
 }
